d1f485517130cf6eaf6fc39bd9376c1811bfbceb
[Github <tmddms313@naver.com>
2022-04-27 01:26:59 +0900

    Apply Material UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,49 +1,22 @@
 import React from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 
 /* 이때 customer Component는 React.Component로 작성이 된 클래스*/
 
 class Customer extends React.Component {
   render() {
     return (
-      <div>
-        <CustomerProfile
-          id={this.props.id}
-          image={this.props.image}
-          name={this.props.name}
-        />
-        <CustomerInFo
-          birthday={this.props.birthday}
-          gender={this.props.gender}
-          job={this.props.job}
-        />
-      </div>
-    );
-  }
-}
-
-/*이때 alt 속성은 이미지에 대한 설명 */
-
-class CustomerProfile extends React.Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.image} alt="profile" />
-        <h2>
-          {this.props.name}({this.props.id})
-        </h2>
-      </div>
-    );
-  }
-}
-
-class CustomerInFo extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>{this.props.birthday}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.job}</p>
-      </div>
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img scr={this.props.image} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
     );
   }
 }
