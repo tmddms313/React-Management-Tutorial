f1b84a88bb9c47ab8eea38f689bf82d8ccbc0cb1
[Github <tmddms313@naver.com>
2022-04-28 01:23:55 +0900

    finally

--- server.js
+++ server.js
@@ -2,39 +2,52 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
+const fs = require("fs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+connection.connect();
+
+const multer = require("multer");
+const upload = multer({ dex: "./upload" });
+
 /*고객 정보 불러오는 코드 */
 
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "이승은",
-      birthday: "000408",
-      gender: "여자",
-      job: "학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "정아윤",
-      birthday: "001108",
-      gender: "여자",
-      job: "학생",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "홍송미",
-      birthday: "001208",
-      gender: "여자",
-      job: "학생",
-    },
-  ]);
+  res.send();
+  "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    };
+});
+
+app.use("/image", express.static("./upload"));
+
+app.post("/api/customers", upload.single("image"), (req, res) => {
+  let sql = "INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)";
+  let image = "/image" + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params, (err, rows, fields) => {
+    res.send(rows);
+    console.log(err);
+    console.log(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
